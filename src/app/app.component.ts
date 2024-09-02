efdf4eea9be1f877399708221cf9c12d96f77269
Sushanta Bhowmick <sushanta.xotiv@gmail.com>
2024-09-02 19:11:11 +0530

    add date format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,15 @@ import { MultiSelectComponent } from './components/multi-select-dropdown/multi-s
 import { SelectComponent } from './components/multi-select-dropdown/select/select.component';
 import { DatePickerComponent } from "./components/Dates/date-picker/date-picker.component";
 import { MatreialDatesComponent } from './components/Dates/matreial-dates/matreial-dates.component';
+import {  TranslateModule } from '@ngx-translate/core';
+import { PopupDatePickerComponent } from "./components/Dates/popup-date-picker/popup-date-picker.component";
+import { DateTimeService } from './services/date-time/date-time.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-root',
@@ -19,6 +28,7 @@ import { MatreialDatesComponent } from './components/Dates/matreial-dates/matrei
   imports: [
     RouterOutlet,
     CKEditorModule,
+    FormsModule,
     MultiSelectComponent,
     SelectComponent,
     HighchartsComponent,
@@ -26,14 +36,46 @@ import { MatreialDatesComponent } from './components/Dates/matreial-dates/matrei
     D3chartsAgComponent,
     TextEditorComponent,
     TexteditorQuillComponent,
+    ReactiveFormsModule,
     QuillComponent,
     KolkovEditorComponent,
     DatePickerComponent,
-    MatreialDatesComponent
+    MatreialDatesComponent,
+    TranslateModule,
+    PopupDatePickerComponent,
+    CommonModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatDatepickerModule
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'trykit';
+
+  public selectedFormat!: string;
+  public formatList =
+  [
+    'DD-MM-YYYY',
+    'DD/MM/YY',
+    'L',
+    'LL',
+  ];
+
+  public constructor(
+    private _dateTimeService: DateTimeService)
+  {
+    console.log("App.component - ctor");
+  }
+
+  public ngOnInit(): void
+  {
+    this.selectedFormat = this._dateTimeService.format;
+  }
+
+  public onSelectionChange_Format(event:any): void
+  {
+    this._dateTimeService.format = this.selectedFormat;
+  }
 }
